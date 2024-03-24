1c5193c3aede0181681a586a4b4e047d36229a3b
Hayato Kawai <fohte.hk@gmail.com>
2024-03-24 11:25:45 +0900

    perf(app/top/pickup): load only 5 features first

--- workspaces/app/src/pages/TopPage/internal/Pickup.tsx
+++ workspaces/app/src/pages/TopPage/internal/Pickup.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react';
+import { useId, useState } from 'react';
 import { FeatureCard } from '../../../features/feature/components/FeatureCard';
 import { useFeatureList } from '../../../features/feature/hooks/useFeatureList';
 import { Box } from '../../../foundation/components/Box';
@@ -8,8 +8,16 @@ import { Text } from '../../../foundation/components/Text';
 import { Color, Space, Typography } from '../../../foundation/styles/variables';
 
 export const Pickup: React.FC = () => {
-  const { data: featureList } = useFeatureList({ query: {} });
+  const { data: allFeatureList } = useFeatureList({ query: {} });
+  const [featureList, setFeatureList] = useState<typeof allFeatureList>(allFeatureList.slice(0, 5));
+  const [currentIndex, setCurrentIndex] = useState(0);
   const pickupA11yId = useId();
+  const nextFeatures = () => {
+    const nextIndex = currentIndex + 5;
+    setFeatureList(allFeatureList.slice(nextIndex, nextIndex + 5));
+    setCurrentIndex(nextIndex);
+  };
+
   return (
     <Box aria-labelledby={pickupA11yId} as="section" maxWidth="100%" mt={16} width="100%">
       <Text as="h2" color={Color.MONO_100} id={pickupA11yId} typography={Typography.NORMAL20} weight="bold">
@@ -21,6 +29,13 @@ export const Pickup: React.FC = () => {
           {featureList.map((feature) => (
             <FeatureCard key={feature.id} bookId={feature.book.id} />
           ))}
+          <button
+            onClick={() => {
+              nextFeatures();
+            }}
+          >
+            続き
+          </button>
         </Flex>
       </Box>
     </Box>
